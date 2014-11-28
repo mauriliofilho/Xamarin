636795d209050823abc205906e172323f529180d
Maurilio Filho <mauriliofilho22@gmail.com>
2014-11-28 01:41:20 -0200

    Nova Modificação do layout do app
    Utilização das duas tecnicas de inserção de controles no layout, usando
    primeiro diretamente via hardcode, e outro usando os resourses no
    arquivo string.

---
+++ Lab1/Ola_codResourceString/Ola_codResourceString/MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Ola_codResourceString
+{
+	[Activity (Label = "Ola_codResourceString", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			//Cria a Interface do usuario com o codigo
+			var layout = new LinearLayout(this);
+			layout.Orientation = Orientation.Vertical;
+
+			var aLabel = new TextView (this);
+			aLabel.SetText(Resource.String.helloLabelText);
+
+			var aButton = new Button (this);
+			aButton.SetText(Resource.String.helloButtonText);
+			aButton.Click += (sender, e) => 
+			{
+				aLabel.Text = "Voce CLicou nesse Botao";
+			};
+
+
+			layout.AddView (aLabel);
+			layout.AddView (aButton);
+
+			SetContentView (layout);
+
+
+		}
+	}
+}
+
+
+
---
+++ Lab1/Ola_codResourceString/Ola_codResourceString/aLabel.cs
@@ -0,0 +1,19 @@
+using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Ola_codResourceString
+{
+	class aLabel
+	{
+	}
+
+}
+
+
+
